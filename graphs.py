3d86e9a2fe7206b86f1f752af94a9d165e4d3bc0
Artur Martins <arturmartinspaiva@gmail.com>
2024-09-26 18:14:06 -0300

    Processing data

--- graphs.py
+++ graphs.py
@@ -7,25 +7,42 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 
 data = pd.read_csv('student_performance_prediction.csv')
-
-
-# Tratar valores faltantes, se houver (exemplo: preencher com a média)
-data['Study Hours per Week'] = data['Study Hours per Week'].fillna(data['Study Hours per Week'].mean())
-# Colocar valores infinitos como Nulos
-data['Study Hours per Week'] = data['Study Hours per Week'].replace([np.inf, -np.inf], np.nan)
-
-(data.head())
-
-(data.info())
-
-(data.describe())
-
 # Criando uma cópia dos dados para manipulação, mantendo os dados originais intactos
 copyData = data.copy()
 
-# Assinalando valores para dados não numéricos
+# Tratar valores numéricos nulos, trocando o nulo pelo valor da média
+count = 0
+for variavel in copyData.columns:
+    if count == 0:
+        count += 1
+    elif count < 4:
+        copyData[variavel] = copyData[variavel].fillna(copyData[variavel].mean())
+        count += 1
+
+    
+# Colocar valores infinitos como Nulos
+#copyData['Study Hours per Week'] = copyData['Study Hours per Week'].replace([np.inf, -np.inf], np.nan)
+
+# Assinalando valores para dados não numéricos (variáveis categóricas)
 copyData['Passed'] = copyData['Passed'].map({'Yes': 1, 'No': 0})
 copyData['Participation in Extracurricular Activities'] = copyData['Participation in Extracurricular Activities'].map({'Yes': 1, 'No': 0})
+# Nível de escolaridade dos pais é uma variável categórica nominal
+dummies = pd.get_dummies(copyData['Parent Education Level'])
+ohe = OneHotEncoder()
+feature_array = ohe.fit_transform(copyData[['Parent Education Level']]).toarray()
+# Visualizar as categorias da variável em colunas
+feature_labels = ohe.categories_
+# Criando um array
+feature_labels = np.array(feature_labels).ravel()
+# Transformando os arrays em um data frame
+features = pd.DataFrame(feature_array, columns = feature_labels)
+
+# Juntando a variavel dummy e as colunas OHE para a base de dados original
+data_new = pd.concat([copyData, dummies, features], axis=1)
+data_new = data_new.drop(columns='Parent Education Level', axis=1)
+
+print(data_new.head())
+
 
 # Histograma
 #plt.figure(figsize=(8,6))
