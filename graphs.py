50d93b59f321d79e277ce71f4dee727be0fb338b
Artur Martins <arturmartinspaiva@gmail.com>
2024-10-03 11:11:44 -0300

    fixed student id column

--- graphs.py
+++ graphs.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import re
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
@@ -12,6 +13,8 @@ data = pd.read_csv('student_performance_prediction.csv')
 # Criando uma cópia dos dados para manipulação, mantendo os dados originais intactos
 copyData = data.copy()
 
+# TRATAMENTO    DE      DADOS
+
 # Tratar variáveis numéricas de valor nulo, substituindo pelo valor da média
 count = 0
 for variavel in copyData.columns:
@@ -20,6 +23,8 @@ for variavel in copyData.columns:
     elif count < 4:
         copyData[variavel] = copyData[variavel].fillna(copyData[variavel].mean())
         count += 1
+    elif count == 5:
+        copyData[variavel] = copyData[variavel].fillna()
     
 # Colocar variáveis infinitas como Nulos
 #copyData['Study Hours per Week'] = copyData['Study Hours per Week'].replace([np.inf, -np.inf], np.nan)
@@ -28,9 +33,17 @@ for variavel in copyData.columns:
 copyData['Passed'] = copyData['Passed'].map({'Yes': 1, 'No': 0})
 copyData['Participation in Extracurricular Activities'] = copyData['Participation in Extracurricular Activities'].map({'Yes': 1, 'No': 0})
 
+# Tirar o "S" no início dos students ID 
+copyData['Student ID'] = copyData['Student ID'].astype(str)
+copyData2 = copyData[copyData['Student ID'].str.match(r'S\d{5}')]
+copyData2.loc[:, 'Student ID'] = copyData2['Student ID'].apply(lambda x: re.sub(r'S', '', x))
+
+
+# TRANSFORMAR   NUM     DATAFRAME
+
 # Transformar variáveis categóricas em uma matriz de variáveis dummy e depois num array numpy
 ohe = OneHotEncoder()
-feature_array = ohe.fit_transform(copyData[['Parent Education Level']]).toarray()
+feature_array = ohe.fit_transform(copyData2[['Parent Education Level']]).toarray()
 
 # Visualizar as categorias da variável em colunas
 feature_labels = ohe.categories_
@@ -42,7 +55,7 @@ feature_labels = np.array(feature_labels).ravel()
 features = pd.DataFrame(feature_array, columns = feature_labels)
 
 # Juntando a variavel dummy e as colunas OHE para a base de dados original e deleta a original
-dataNew = pd.concat([copyData, features], axis=1)
+dataNew = pd.concat([copyData2, features], axis=1)
 dataNew = dataNew.drop(columns='Parent Education Level', axis=1)
 
 # Colocar a coluna 'Passed' como a última
@@ -54,35 +67,35 @@ colunas.append('Passed')
 # Reordenamos o DataFrame com base nas novas colunas
 dataNew = dataNew[colunas]
 
-# Visualizando o resultado
-# print(dataNew.head())
+
+#   DEFINIR     VARIÁVEIS     EXPLICATIVAS  E   ALVO
+
 
 # Definindo as variáveis explicativas e a variável alvo
 y = dataNew.Passed
 x = dataNew.drop(columns='Passed', axis=1)
-# print(y)
-# print(x)
+print(y)
+print(x)
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 2)
-# print(x.shape, x_train.shape, x_test.shape)
+print(x.shape, x_train.shape, x_test.shape)
 
 # Linear Regression
 reg = LinearRegression()
-# reg.fit(x_train.values, y_train.values)
+reg.fit(x_train.values, y_train.values)
+
 
 # Prevendo pelos dados treinados
 train_data_pred = reg.predict(x_train.values)
 
 # R squared value
 r2_train = metrics.r2_score(y_train, train_data_pred)
-#print('r2 squared value: ', r2_train)
-
-
+print('r2 squared value: ', r2_train)
 
 
 # Histograma
 #plt.figure(figsize=(8,6))
-#sns.histplot(copyData['Study Hours per Week'], bins=15, kde=True, color='blue')
+#sns.histplot(dataNew['Study Hours per Week'], bins=15, kde=True, color='blue')
 #plt.title('Distribuição das Horas de Estudo por Semana')
 #plt.xlabel('Horas de Estudo por Semana')
 #plt.ylabel('Frequência')
@@ -90,17 +103,17 @@ r2_train = metrics.r2_score(y_train, train_data_pred)
 
 # Gráfico de plot
 #sns.relplot(
-#    data=copyData,
+#    data=dataNew,
 #    x="Previous Grades", y="Study Hours per Week", col="Passed", hue="Attendance Rate")
 #plt.show()
 
 # Distribuição da variável de frequência
-#sns.displot(copyData['Attendance Rate'])
+#sns.displot(dataNew['Attendance Rate'])
 #plt.title('Frequência')
 #plt.show()
 
 # Distribuição da variável de nível de escolaridade dos pais
-#educationLevelGraph = sns.countplot(x='Parent Education Level', data=copyData)
+#educationLevelGraph = sns.countplot(x='Parent Education Level', data=dataNew)
 #plt.title('Escolaridade dos pais')
 
 # Mostrar valores exatos para cada nível de escolaridade
@@ -110,3 +123,6 @@ r2_train = metrics.r2_score(y_train, train_data_pred)
 #                textcoords='offset points')
 #plt.show()
 
+
+
+# DO LOGISTIC REGRESSION, TREE MODELS, KNM MODELS, SUPPORT VECTOR, RANDOM FOREST
